c5112a3aa592f946d77739f28a87570da558345b
EmmaGaRu <105665115+EmmaGaRu@users.noreply.github.com>
2022-05-16 21:51:40 +0200

    Add files via upload

---
+++ Challenged 1. Find maximum.java
@@ -0,0 +1,15 @@
+class Main {
+public static void main(String[] args) {
+        Integer[] numList = new Integer[]{1, 2, 3, 4, 5};
+        System.out.println("El número máximo es: " + maximum(numList));
+    }
+    public static Integer maximum(Integer[] numList) {
+        int mayor = numList[0];
+        for (int i = 1; i < numList.length; i++) {
+            if (numList[i] > mayor) {
+                mayor = numList[i];
+            }
+        }
+        return mayor;
+    }
+}
